315c2534faecb955c6bb251d7a5fac87fc6b5523
Ian Perfitt <iancp@protonnmail.com>
2023-11-10 13:17:21 -0600

    implemented activated router to capture the ID

--- src/app/anti-hero/pages/form/form.component.ts
+++ src/app/anti-hero/pages/form/form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-form',
@@ -6,10 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form.component.scss']
 })
 export class FormComponent implements OnInit {
-
-  constructor() { }
+  id = "";
+  constructor(private router: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.id = this.router.snapshot.params['id'];
   }
 
 }
