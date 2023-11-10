719e8db286a516880061736dcaf8a0cd392cbd0e
Ian Perfitt <iancp@protonnmail.com>
2023-11-10 13:15:37 -0600

    implemented clear(), emitAction(), checkAction(). Implemented form, selectedId, actionButtonLabel and action eventemitter

--- src/app/anti-hero/components/anti-hero-form/anti-hero-form.component.ts
+++ src/app/anti-hero/components/anti-hero-form/anti-hero-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-anti-hero-form',
@@ -6,10 +7,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./anti-hero-form.component.scss']
 })
 export class AntiHeroFormComponent implements OnInit {
+  @Input() selectedId = "";
+  @Input() actionButtonLabel: string = 'Create';
+  @Output() action = new EventEmitter();
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      id: [''],
+      firstName: [''],
+      lastName: [''],
+      house: [''],
+      knownAs: ['']
+    })
+   }
 
   ngOnInit(): void {
+    this.checkAction();
   }
 
+  checkAction() {
+    if(this.selectedId) {
+      this.actionButtonLabel = "Update";
+      this.patchDataValues()
+    }
+  }
+
+  patchDataValues () {
+    //this will be implemented in the future (for update feature)
+   // this.form.patchValue();
+  }
+
+  emitAction() {
+    this.action.emit({value: this.form.value, action: this.actionButtonLabel})
+  }
+
+  clear() {
+     this.form.reset();
+  }
 }
