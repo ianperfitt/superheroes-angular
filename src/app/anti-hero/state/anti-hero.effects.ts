5c65472724d8c15593a5ed08faac5d4d19cd4548
Ian Perfitt <iancp@protonnmail.com>
2023-11-10 23:28:10 -0600

    implemented effects for getting the list of aanti-heroes

--- src/app/anti-hero/state/anti-hero.effects.ts
+++ src/app/anti-hero/state/anti-hero.effects.ts
@@ -1,10 +1,23 @@
 import { Injectable } from "@angular/core";
 import { Router } from "@angular/router";
-import { Actions } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { AntiHeroService } from "../services/anti-hero.service";
+import { AntiHeroActions } from "./anti-hero.actions";
+import { EMPTY, catchError, map, mergeMap } from "rxjs";
 
 @Injectable()
 export class AntiHeroEffects {
+
+    getAntiHeroes$ = createEffect(() => {
+        return this.actions$.pipe(
+            ofType(AntiHeroActions.GET_ANTI_HERO_LIST),
+            mergeMap(() => this.antiHeroService.getAntiHeroes()
+            .pipe(
+                map(antiHeroes => ({ type: AntiHeroActions.SET_ANTI_HERO_LIST, antiHeroes})),
+                catchError(() => EMPTY)
+            ))
+        )
+    }, {dispatch: true});
     constructor(
         private actions$: Actions,
         private antiHeroService: AntiHeroService,
