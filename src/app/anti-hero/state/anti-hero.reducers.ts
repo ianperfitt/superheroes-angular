e28855d68b5f5804d7804daa0d9649495797b1a9
Ian Perfitt <iancp@protonnmail.com>
2023-11-13 12:40:18 -0600

    added new reducer for delete feature in anti-hero.reducers.ts

--- src/app/anti-hero/state/anti-hero.reducers.ts
+++ src/app/anti-hero/state/anti-hero.reducers.ts
@@ -1,6 +1,6 @@
 import { createReducer, on } from '@ngrx/store';
 import { AntiHero } from '../models/anti-hero.interface';
-import { setAntiHeroList } from './anti-hero.actions';
+import { removeAntiHeroState, setAntiHeroList } from './anti-hero.actions';
 
 export interface AntiHeroState {
     antiHeroes: ReadonlyArray<AntiHero>;
@@ -13,5 +13,9 @@ export const initialState: AntiHeroState = {
 export const antiHeroReducer = createReducer(
   initialState,
   on(setAntiHeroList, (state, { antiHeroes }) => { return {...state, antiHeroes}}),
+  on(removeAntiHeroState, (state, { antiHeroId}) => {
+    return {...state, antiHeroes:
+    state.antiHeroes.filter(data => data.id != antiHeroId)}
+  })
 
   );
