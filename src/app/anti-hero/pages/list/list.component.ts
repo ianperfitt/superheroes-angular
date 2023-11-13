d0bce327a332b7a73e39784a514b9e7025b2dce0
Ian Perfitt <iancp@protonnmail.com>
2023-11-13 13:14:17 -0600

    implemented case for TableActions.Delete in list.component.ts

--- src/app/anti-hero/pages/list/list.component.ts
+++ src/app/anti-hero/pages/list/list.component.ts
@@ -41,7 +41,19 @@ export class ListComponent implements OnInit {
   }
 
   selectAntiHero(data: {antiHero: AntiHero, action: TableActions}) {
-    this.router.navigate(['anti-heroes', 'form', data.antiHero.id]);
+    switch(data.action) {
+      case TableActions.View: {
+        this.router.navigate(['anti-heroes', 'form', data.antiHero.id]);
+        return;
+      }
+      case TableActions.Delete: {
+        this.store.dispatch({type:
+          AntiHeroActions.REMOVE_ANTI_HERO_STATE,
+          antiHeroId: data.antiHero.id});
+        return;
+      }
+      default: ""
+    }
   }
 
   executeCommandBarAction(action: CommandBarActions) {
