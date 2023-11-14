f458f83ca901b6fe0f98e23bb58e7de6aff779aa
Ian Perfitt <iancp@protonnmail.com>
2023-11-13 20:29:51 -0600

    created form action and added as action to form.component.html

--- src/app/anti-hero/pages/form/form.component.ts
+++ src/app/anti-hero/pages/form/form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AntiHeroActions } from '../../state/anti-hero.actions';
+import { AntiHero } from '../../models/anti-hero.interface';
 
 @Component({
   selector: 'app-form',
@@ -8,10 +10,23 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class FormComponent implements OnInit {
   id = "";
+  store: any;
   constructor(private router: ActivatedRoute) { }
 
   ngOnInit(): void {
     this.id = this.router.snapshot.params['id'];
+  }
+
+  formAction(data: {value: AntiHero, action: string}) {
+    switch(data.action) {
+      case "Create" : {
+        this.store.dispatch({type:
+          AntiHeroActions.ADD_ANTI_HERO_API,
+          payload: data.value});
+        return;
+      }
+      default: ""
+    }
   }
 
 }
