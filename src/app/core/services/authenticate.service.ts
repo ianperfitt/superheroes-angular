f937f3e77ff55de6c23352a5f2ca02e554131b45
Ian Perfitt <iancp@protonnmail.com>
2023-11-14 15:34:48 -0600

    implemented login & register functions

--- src/app/core/services/authenticate.service.ts
+++ src/app/core/services/authenticate.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, tap, throwError } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticateService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(data: {email: string, password: string}):
+    Observable<any> {
+      return this.http.post<any>(
+        `${environment.authURL}/authenticate`,
+        data).pipe(
+          tap((data: any) => data),
+          catchError(err => throwError(() => err))
+        )
+    }
+
+    register(data: {email: string, password: string}):
+      Observable<any> {
+        return this.http.post<any>(
+          `${environment.authURL}/register`, data).pipe(
+            tap((data: any) => data),
+            catchError(err => throwError(() => err))
+          )
+      }
 }
