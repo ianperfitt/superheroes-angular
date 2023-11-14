8c863f2b27f45ad24eca0454aaa78a8d57fecb8d
Ian Perfitt <iancp@protonnmail.com>
2023-11-14 16:25:04 -0600

    added following imports: MaterialModule,

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CoreModule } from '../core/core.module';
+import { MaterialModule } from '../material/material.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CoreModule
+  ]
+})
+export class AuthModule { }
