9c99e4a81c850d7bd141116bfecbdf8f3a96a811
Ian Perfitt <iancp@protonnmail.com>
2023-11-14 16:59:19 -0600

    implemented register.component.ts and craete User model

--- src/app/auth/page/register/register.component.ts
+++ src/app/auth/page/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticateService } from 'src/app/core/services/authenticate.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
+  error: string = "";
 
-  constructor() { }
+  constructor(private authService: AuthenticateService, private router: Router) {
 
-  ngOnInit(): void {
   }
 
+  submit(data: User) {
+    this.authService.register(data).subscribe((data) => {
+      this.router.navigate(['/']);
+    });
+  }
 }
