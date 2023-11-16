ed8b66f5876ac64ac3d4ba4e6c2063fcc3ba729b
Ian Perfitt <iancp@protonnmail.com>
2023-11-15 21:20:35 -0600

    applied AuthGuard in the anti-heroes route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {
@@ -16,6 +17,7 @@ const routes: Routes = [
     path: "anti-heroes",
     loadChildren: () =>
       import("./anti-hero/anti-hero.module").then((m) => m.AntiHeroModule),
+      canLoad: [AuthGuard]
   }
 
 ];
