d3d4044a85065a22175df3d7ee208345f19772c1
Ian Perfitt <iancp@protonnmail.com>
2023-11-16 14:41:48 -0600

    created a reducer that calls localStorageSync

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -7,7 +7,16 @@ import { AuthFormComponent } from './components/auth-form/auth-form.component';
 import { LoginComponent } from './page/login/login.component';
 import { RegisterComponent } from './page/register/register.component';
 import { AuthRoutingModule } from './auth-routing.module';
+import { ActionReducer, MetaReducer, StoreModule } from '@ngrx/store';
+import { localStorageSync } from 'ngrx-store-localstorage';
+import { AuthEffects } from './state/auth.effects';
+import { authReducer } from './state/auth.reducers';
+import { EffectsModule } from '@ngrx/effects';
 
+export function localStorageSyncReducer(reducer: ActionReducer<any>): ActionReducer<any> {
+  return localStorageSync({keys: ['token']})(reducer);
+}
+const metaReducers: Array<MetaReducer<any, any>> = [localStorageSyncReducer];
 
 
 @NgModule({
@@ -18,11 +27,13 @@ import { AuthRoutingModule } from './auth-routing.module';
   ],
   imports: [
     CommonModule,
+    AuthRoutingModule,
     MaterialModule,
     FormsModule,
     ReactiveFormsModule,
-    AuthRoutingModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forFeature('authState', authReducer, {metaReducers}),
+    EffectsModule.forFeature([AuthEffects]),
   ]
 })
 export class AuthModule { }
