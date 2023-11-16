af4593d723924e0096ed14386c168e62e6e5eb39
Ian Perfitt <iancp@protonnmail.com>
2023-11-15 21:35:22 -0600

    created and implemented FormGuard

--- src/app/anti-hero/pages/form/form.component.ts
+++ src/app/anti-hero/pages/form/form.component.ts
@@ -4,16 +4,23 @@ import { AntiHeroActions } from '../../state/anti-hero.actions';
 import { AntiHero } from '../../models/anti-hero.interface';
 import { AppState } from 'src/app/state/app.state';
 import { Store } from '@ngrx/store';
+import { CanComponentDeactivate } from 'src/app/core/guards/form.guard';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
-export class FormComponent implements OnInit {
+export class FormComponent implements OnInit, CanComponentDeactivate{
   antiHero: AntiHero | null = null;
   
   constructor(private router: ActivatedRoute, private store: Store<AppState>) { }
+  
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    const confirmation = window.confirm('Are you sure?');
+    return confirmation;
+  }
 
   ngOnInit(): void {
   }
